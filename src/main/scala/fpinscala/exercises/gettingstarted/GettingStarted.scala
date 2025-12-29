f0629c00c6d5b28d50e57ec2ec01b32ab82b7350
aebel-shajan <aebelshajan@gmail.com>
2025-12-29 15:48:27 +0000

    Complete exercise 2.2

--- src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
@@ -125,7 +125,14 @@ object PolymorphicFunctions:
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean =
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if n >= as.length-1 then true
+      else if gt(as(n), as(n+1)) then false
+      else go(n+1)
+
+    go(0)
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
